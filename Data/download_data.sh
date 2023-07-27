d252f2a55e2bbf01c6fc11f6cff06d8b74546f3b
Derrik Gratz <derrik.gratz@gmail.com>
2023-07-27 16:21:34 -0400

    init data download script

---
+++ Data/download_data.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+## Metadata
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE135nnn/GSE135893/suppl/GSE135893%5FIPF%5Fmetadata%2Ecsv%2Egz
+## Barcodes
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE135nnn/GSE135893/suppl/GSE135893%5Fbarcodes%2Etsv%2Egz
+## Genes
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE135nnn/GSE135893/suppl/GSE135893%5Fgenes%2Etsv%2Egz
+## counts matrix
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE135nnn/GSE135893/suppl/GSE135893%5Fmatrix%2Emtx%2Egz
