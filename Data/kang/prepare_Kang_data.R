eaf9aa6b08f90a5a0824834020b0ce1413e723e2
Derrik Gratz <derrik.gratz@gmail.com>
2023-10-04 10:05:56 -0400

    simplify data gen script

--- Data/kang/prepare_Kang_data.R
+++ Data/kang/prepare_Kang_data.R
@@ -1,31 +1,17 @@
----
-title: "preparing data"
-author: "ENPRC Genomics Core"
-date: "`r Sys.Date()`"
-output: html_document
----
-
-```{r setup, include=FALSE}
 library(DropletUtils)
 library(Seurat)
 library(here)
-knitr::opts_chunk$set(echo=FALSE,
-                      warning=TRUE,
-                      message=TRUE,
-                      include=TRUE,
-                      fig.width = 8, fig.height = 8,
-                      fig.align = 'center',
-                      cache=FALSE, cache.lazy = FALSE)
-```
+library(scater)
+library(Seurat)
 
-```{r}
+## This is only kept for record keeping purposes for how data was generated
+## You don't need to run this
+
 kang.sce <- muscData::Kang18_8vs8()
 kang.sce <- scater::logNormCounts(kang.sce)
 kang <- Seurat::as.Seurat(kang.sce)
 kang$stim <- as.character(kang$stim)
-```
 
-```{r}
 for (individual in unique(kang$ind)) {
   for (cond in c('ctrl', 'stim')){
     kang.sub <- subset(kang, subset = ind == individual & stim == cond)
@@ -34,19 +20,16 @@ for (individual in unique(kang$ind)) {
                                  type='HDF5', version="3", overwrite = TRUE)
   }
 }
-```
 
-```{r}
+## Testing all subsets ----
+
 for (individual in unique(kang$ind)) {
   for (cond in c('ctrl', 'stim')){
     name <- paste0(individual, '_', cond)
     obj <- Read10X_h5(here::here(paste0('Data/kang/s', name,'.h5')))
     obj <- CreateSeuratObject(obj)
-    rmarkdown::render(here::here('Session-02/02-processing_template.runfile.Rmd'),
-                  output_dir = here('Session-02/test_outputs'), 
-                  output_file = paste0(name, '.html'))
+    rmarkdown::render(here::here('Session-02/02-QC_processing.Rmd'),
+                      output_dir = here('Session-02/test_outputs'),
+                      output_file = paste0(name, '.html'))
   }
 }
-
-```
-
